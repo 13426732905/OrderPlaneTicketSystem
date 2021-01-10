15d613156c328141b4d706065dbc926843849789
0110 <1926309964@qq.com>
2021-01-10 21:54:54 +0800

    类，封装，继承

--- 20210110/src/com/company/Student.java
+++ 20210110/src/com/company/Student.java
@@ -1,53 +1,7 @@
 package com.company;
 
-public class Student {
-
-    private  int  Id;  //学号
-    private  String name; //姓名
-    private  String  password; //密码
-    private  String phone;    //电话号码
-    private  String address;  //地址
+public class Student  extends  User{
     private  String major;    //专业
-
-    public int getId() {
-        return Id;
-    }
-
-    public void setId(int id) {
-        Id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
 
     public String getMajor() {
         return major;
---
+++ 20210110/src/com/company/User.java
@@ -0,0 +1,50 @@
+package com.company;
+
+public class User {
+
+    private  int  Id;  //学号
+    private  String name; //姓名
+    private  String  password; //密码
+    private  String phone;    //电话号码
+    private  String address;  //地址
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
