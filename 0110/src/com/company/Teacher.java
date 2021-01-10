fe26b0b76d663dee83d2a8be6ce572b0609a7645
0110 <1926309964@qq.com>
2021-01-10 15:26:45 +0800

    封装和继承

---
+++ 0110/src/com/company/Teacher.java
@@ -0,0 +1,15 @@
+package com.company;
+
+
+
+public class Teacher extends  User {
+    private  String chichen;
+
+    public String getChichen() {
+        return chichen;
+    }
+
+    public void setChichen(String chichen) {
+        this.chichen = chichen;
+    }
+}
---
+++ 0110/src/com/company/student.java
@@ -0,0 +1,33 @@
+package com.company;
+
+import java.util.StringTokenizer;
+
+public class student extends User{
+     private String stuname;
+     private  String phone;
+     private  String address;
+    public String getStuname() {
+        return stuname;
+    }
+
+    public void setStuname(String stuname) {
+        this.stuname = stuname;
+    }
+
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
